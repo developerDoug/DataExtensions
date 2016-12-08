e7d2902458d42d2060f6803264d3cc00d7a8981c
DESKTOP-29RNN4I\demason <androidsoong@gmail.com>
2016-12-08 08:02:44 -0600

    Added DataRowExtensions class to more easily get a value of a specific type and have it default to a value when not found.

---
+++ DataExtensions/DataRowExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+namespace DataExtensions
+{
+    public static class DataRowExtensions
+    {
+        public static TReturnType ConvertTo<TReturnType>(this DataRow row, string columnName, TReturnType defaultValue = default(TReturnType))
+        {
+            ThrowExceptionIfRowIsNull(row);
+
+            return row[columnName] == DBNull.Value
+                ? defaultValue
+                : row.Field<TReturnType>(columnName);
+        }
+
+        private static void ThrowExceptionIfRowIsNull(DataRow row)
+        {
+            if (row == null)
+            {
+                throw new NullReferenceException("The DataRow was null; could not convert value to specified type.");
+            }
+        }
+    }
+}
